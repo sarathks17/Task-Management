6db762a720d08126cfaf7a443d4654e55eb67d1c
sarathkumar s <sarathkumar93622@gmail.com>
2025-07-22 21:00:54 +0530

    frontend structure finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+    <div className='form-container'>
+      <form>
+        <div className='input-container'>
+          <input type="text" placeholder='Add New Task'/>
+          <button>Add Task</button>
+        </div>
+      </form>
+    </div>
+    <div className='card-container'>
+            <h4>Card Title</h4>
+            <p>sample description about the card</p>
+            <h5>Due Date: 22-10-2000</h5>
+            <h4>Status: Pending</h4>
+           <div className='card-btn'>
+            <button>Update</button>
+            <button>Delete</button> 
+           </div>
+    </div>
+    </div>
   )
 }
 
